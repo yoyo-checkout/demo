d70893c6393096e49059284ae79a88ed215e858c
zongyo <stem597sag445@gmail.com>
2020-03-17 22:31:12 +0800

    init vue dev env

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>this is header</header>
+    <aside>this is aside</aside>
+    <main>this is content</main>
+    <footer>this is footer</footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
