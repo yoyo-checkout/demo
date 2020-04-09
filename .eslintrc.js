a78dea4a3bd9d49e0db353cedb24795f6a00688e
zongyo <stem597sag445@gmail.com>
2020-04-09 23:39:36 +0800

    init unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -19,4 +23,16 @@ module.exports = {
     'vue/no-unused-vars': 'off',
     'vue/require-v-for-key': 'off',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
 };
---
+++ tests/unit/covid19/DateSlider.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-expressions */
+
+import { expect } from 'chai';
+// import { shallowMount } from '@vue/test-utils';
+// import DateSlider from '@/components/covid19/DateSlider.vue';
+
+describe('DateSlider.vue', () => {
+  it('renders props.msg when passed', () => {
+    // const wrapper = shallowMount(DateSlider);
+    // expect(wrapper.vm.$store.Covid19.dateRange).to.be.an('array');
+    expect(true).to.be.true;
+  });
+});
